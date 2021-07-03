6d270228647b59a151c5c3e21ce78a7b9c4ffa8d
rem657 <33555579+rem657@users.noreply.github.com>
2021-07-02 22:07:33 -0400

    MTGCardPricer V1.0 The program is able to generate a price tag for a list of given magic the gathering cards

--- Card.py
+++ Card.py
@@ -3,14 +3,16 @@ import time
 
 
 class Card:
-    def __init__(self, card_name: str, card_set: str):
+    def __init__(self, card_name: str, card_set: str, foil=False, quantity:int = 1):
         self.browser = self.open_web_chrome('https://www.facetofacegames.com/')
         # self.name = card_name
         # self.set = card_set
         self.price_ftf = 0.0
         self.name = ''
         self.set = ''
-        self.get_price(card_name, card_set)
+        self.foil = None
+        self.quantity = quantity
+        self.get_price(card_name, card_set, foil)
 
     @staticmethod
     def open_web_chrome(url: str) -> webdriver.Chrome:
@@ -44,32 +46,64 @@ class Card:
         self.browser.find_element_by_xpath(
             '/html/body/header/div/div/div[2]/div[1]/div[2]/form/fieldset/div/button').click()
 
-    def get_price(self, name: str, set_name: str):
+    def select_singles(self):
+        """
+        This method makes sure
+        :return:
+        """
+        self.browser.find_element_by_xpath('//*[@id="facetedSearch-navList"]/div[1]/div[7]/div[1]').click()
+        product_menu = self.browser.find_element_by_xpath('//*[@id="facetedSearch-navList--Product-Type"]')
+        all_categories = product_menu.find_elements_by_class_name('navList-item')
+        for category in all_categories:
+            if category.text.startswith('Singles'):
+                category.click()
+                break
+        time.sleep(2)
+
+    def get_price(self, name: str, set_name: str, foil: bool = False):
         """
         This method searches for a given card on the website and returns the highest price for the first product
         :param name: A string for the name of the magic the gathering card
         :param set_name: A string for the name of the set in which the card is printed
+        :param foil: A bool for whether or not you want the foil price
         :return: A float for the highest price of the card
         """
         self.search_card(name, set_name)
-        time.sleep(1)
+        time.sleep(1.5)
+        self.select_singles()
         all_product = self.browser.find_elements_by_class_name('product')
         product_1 = all_product[0]
+        card_finishes = product_1.find_elements_by_class_name('finish-option')
+        for finish in card_finishes:
+            foil_attribute = finish.text
+            if foil and (foil_attribute == 'Foil'):
+                finish.click()
+                self.foil = True
+            elif (not foil) and (foil_attribute == 'Non-Foil'):
+                finish.click()
+                self.foil = False
+
         card_name = product_1.find_element_by_xpath('//article/div[1]/h4/a').text
         card_set = product_1.find_element_by_xpath('//article/div[1]/h4/p').text
         all_price = product_1.find_elements_by_class_name('price-section')
         list_prices = []
         for price in all_price:
             current_price = price.find_elements_by_class_name('price')[0].text
-            if current_price[0] == '$':
-                list_prices.append(float(current_price[1:]))
-            else:
-                list_prices.append(float(current_price))
+            if len(current_price) > 0:
+                if current_price[0] == '$':
+                    list_prices.append(float(current_price[1:]))
+                else:
+                    list_prices.append(float(current_price))
         self.price_ftf = max(list_prices)
         self.name = card_name
         self.set = card_set
 
 
+
 if __name__ == '__main__':
-    Card('liliana', '')
+    a = Card('glimmervoid', 'double master')
+    print(a.name)
+    print(a.set)
+    print(a.price_ftf)
+    print(a.foil)
     # MTGCardPricer('force of will borderless','double master')
--- MTGCardPricer.py
+++ MTGCardPricer.py
@@ -1,11 +1,23 @@
-from Card import Card
+from Card import Card, time
 from typing import List
 import pandas as pd
 
 
+def make_card(card_name: str, card_set: str, is_foil: bool, quantity: int):
+    try:
+        new_card = Card(card_name, card_set, is_foil, quantity)
+    except:
+        print('an error occurred trying again soon')
+        time.sleep(2)
+        new_card = make_card(card_name, card_set, is_foil, quantity)
+    return new_card
+
+
 class MTGCardPricer:
     def __init__(self, filename: str):
-        self.cards_data = self.make_card_dataframe()
+        self.cards_data = self.make_card_dataframe(filename)
+        self.calculate_total_price()
+        self.save_to_csv(filename[:-4])
 
     @staticmethod
     def open_and_read(filename: str) -> List[str]:
@@ -18,6 +30,41 @@ class MTGCardPricer:
             list_lines = read_file.readlines()
         return list_lines
 
+    @staticmethod
+    def get_card_from_string(card_string: str):
+        """
+        This method takes a string in the form `quantity`x`cardname`~`cardset`~`card finish` and return a these parameters.
+        quantity, cardset and card finish are optional
+        :param card_string:
+        :return: An int for the quantity, a string of the card name, a string of the card set, a bool whether or not it'
+        s foil.
+        """
+        # checks quantity
+        if card_string[0].isnumeric():
+            quantity_card = card_string.split('x')
+            quantity = int(quantity_card[0])
+            card_string = ''.join(quantity_card[1:])
+        else:
+            quantity = 1
+        # Checks card name
+        name_set = card_string[0:-1].split('~')
+        card_name = name_set[0]
+        # Checks card set
+        if len(name_set) >= 2:
+            card_set = name_set[1]
+        else:
+            card_set = ''
+        if len(name_set) >= 3:
+            card_finish = name_set[2]
+        else:
+            card_finish = ''
+        # check finish
+        if card_finish.capitalize() == 'FOIL':
+            is_foil = True
+        else:
+            is_foil = False
+        return quantity, card_name, card_set, is_foil
+
     def generate_card_objects(self, filename: str) -> List[Card]:
         """
         This method reads the file named `filename` and creates a Card object for each valid card in the file.
@@ -26,15 +73,13 @@ class MTGCardPricer:
         """
         list_lines = self.open_and_read(filename)
         list_card = []
-        for line in list_lines:
+        number_of_entry = len(list_lines)
+        for i, line in enumerate(list_lines):
             if line not in ['', '\n']:
-                name_set = line[0:-1].split('~')
-                card_name = name_set[0]
-                if len(name_set) == 2:
-                    card_set = name_set[1]
-                else:
-                    card_set = ''
-                list_card.append(Card(card_name, card_set))
+                quantity, card_name, card_set, is_foil = self.get_card_from_string(line)
+                new_card = make_card(card_name, card_set, is_foil, quantity)
+                print(f'card done : {i + 1}/{number_of_entry}')
+                list_card.append(new_card)
         return list_card
 
     def make_card_dataframe(self, filename: str) -> pd.DataFrame:
@@ -51,7 +96,37 @@ class MTGCardPricer:
         list_card_name = list(map(lambda card: get_card_attribute(card, 'name'), list_card))
         list_card_set = list(map(lambda card: get_card_attribute(card, 'set'), list_card))
         list_card_price = list(map(lambda card: get_card_attribute(card, 'price_ftf'), list_card))
-        return pd.DataFrame({'name': list_card_name, 'set': list_card_set, 'price_ftf': list_card_price})
+        list_card_quantity = list(map(lambda card: get_card_attribute(card, 'quantity'), list_card))
 
-    def save_to_csv(self):
-        pass
+        return pd.DataFrame({'name': list_card_name, 'quantity': list_card_quantity, 'set': list_card_set,
+                             'price_ftf': list_card_price})
+
+    def calculate_total_price(self):
+        """
+        This method calculates the total price of the given cards and adds a line to the card_data dataframe with this
+         information
+        :return:
+        """
+        index = self.cards_data.index
+        total_price = 0
+        total_quantity = 0
+        for index_num in index:
+            quantity = self.cards_data.loc[index_num, 'quantity']
+            price = self.cards_data.loc[index_num, 'price_ftf']
+            total_price += price * quantity
+            total_quantity += quantity
+        self.cards_data.loc[index[-1] + 1, 'name'] = 'TOTAL'
+        self.cards_data.loc[index[-1] + 1, 'quantity'] = total_quantity
+        self.cards_data.loc[index[-1] + 1, 'price_ftf'] = total_price
+
+    def save_to_csv(self, filename: str):
+        """
+        This method saves the attribute cards_data, which is a dataframe, to a csv file name `filename`
+        :param filename:
+        :return:
+        """
+        self.cards_data.to_csv(f'{filename}.csv')
+
+
+if __name__ == '__main__':
+    MTGCardPricer('sellList.txt')
